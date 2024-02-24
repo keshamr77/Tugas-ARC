d7e5923984716c1fa8d3bc79a4c8eecff6e490b7
keshamr77 <kesha.mr77@gmail.com>
2024-02-24 18:23:34 +0700

    Upload Tugas Day 2

---
+++ Tugas-Website-Day-2/links.js
@@ -0,0 +1,7 @@
+export const links = [
+    {
+        name : "Lokasi Rusdi Barbershop 📍🗺️",
+        link : "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        image: "https://i.pinimg.com/736x/10/22/eb/1022ebfdfc109a02b7564d978ad0b076.jpg"
+    },
+];
